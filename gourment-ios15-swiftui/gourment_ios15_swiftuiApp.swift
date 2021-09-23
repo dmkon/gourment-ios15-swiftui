136201a25602873c82fb2005ef5f2134f8fd073a
Dmitri Kondramachine <dmkon@hotmail.com>
2021-09-23 09:28:32 -0400

    Initial Commit

---
+++ gourment-ios15-swiftui/gourment_ios15_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  gourment_ios15_swiftuiApp.swift
+//  gourment-ios15-swiftui
+//
+//  Created by Dmitri Kondramachine on 2021-09-23.
+//
+
+import SwiftUI
+
+@main
+struct gourment_ios15_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ gourment-ios15-swiftuiTests/gourment_ios15_swiftuiTests.swift
@@ -0,0 +1,33 @@
+//
+//  gourment_ios15_swiftuiTests.swift
+//  gourment-ios15-swiftuiTests
+//
+//  Created by Dmitri Kondramachine on 2021-09-23.
+//
+
+import XCTest
+@testable import gourment_ios15_swiftui
+
+class gourment_ios15_swiftuiTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
